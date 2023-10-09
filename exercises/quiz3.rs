9dfc5ba6f27182f6e8d7d454eccea09e64f10b39
penny <penny.zhang@sap.com>
2023-10-09 18:42:51 +0800

    fix all

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,15 +14,62 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+// pub struct ReportCard {
+//     pub grade: f32,
+//     pub student_name: String,
+//     pub student_age: u8,
+// }
+
+// impl ReportCard {
+//     pub fn print(&self) -> String {
+//         format!("{} ({}) - achieved a grade of {}",
+//             &self.student_name, &self.student_age, &self.grade)
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn generate_numeric_report_card() {
+//         let report_card = ReportCard {
+//             grade: 2.1,
+//             student_name: "Tom Wriggle".to_string(),
+//             student_age: 12,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Tom Wriggle (12) - achieved a grade of 2.1"
+//         );
+//     }
+
+//     #[test]
+//     fn generate_alphabetic_report_card() {
+//         // TODO: Make sure to change the grade here after you finish the exercise.
+//         let report_card = ReportCard {
+//             grade: 2.1,
+//             student_name: "Gary Plotter".to_string(),
+//             student_age: 11,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Gary Plotter (11) - achieved a grade of A+"
+//         );
+//     }
+// }
+
+
+use std::fmt;
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -31,7 +78,7 @@ impl ReportCard {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::* ;
 
     #[test]
     fn generate_numeric_report_card() {
@@ -48,9 +95,9 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
+        // DONE: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,16 +3,73 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+// // Step 1.
+// // Complete the `capitalize_first` function.
+// // "hello" -> "Hello"
+// pub fn capitalize_first(input: &str) -> String {
+//     let mut c = input.chars();
+//     match c.next() {
+//         None => String::new(),
+//         Some(first) => ???,
+//     }
+// }
+
+// // Step 2.
+// // Apply the `capitalize_first` function to a slice of string slices.
+// // Return a vector of strings.
+// // ["hello", "world"] -> ["Hello", "World"]
+// pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+//     vec![]
+// }
+
+// // Step 3.
+// // Apply the `capitalize_first` function again to a slice of string slices.
+// // Return a single string.
+// // ["hello", " ", "world"] -> "Hello World"
+// pub fn capitalize_words_string(words: &[&str]) -> String {
+//     String::new()
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_success() {
+//         assert_eq!(capitalize_first("hello"), "Hello");
+//     }
+
+//     #[test]
+//     fn test_empty() {
+//         assert_eq!(capitalize_first(""), "");
+//     }
+
+//     #[test]
+//     fn test_iterate_string_vec() {
+//         let words = vec!["hello", "world"];
+//         assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+//     }
+
+//     #[test]
+//     fn test_iterate_into_string() {
+//         let words = vec!["hello", " ", "world"];
+//         assert_eq!(capitalize_words_string(&words), "Hello World");
+//     }
+// }
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
+    // Look, Ma! no index.
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            format!("{}{}", first.to_uppercase().to_string(), c.as_str())
+        }
     }
 }
 
@@ -21,15 +78,15 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
-}
+    words.iter().map(|x| capitalize_first(x)).collect()
+ }
 
 // Step 3.
 // Apply the `capitalize_first` function again to a slice of string slices.
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.iter().map(|x| capitalize_first(x)).collect::<Vec<String>>().concat()
 }
 
 #[cfg(test)]
